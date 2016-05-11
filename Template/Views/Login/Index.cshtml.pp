f5427216ce7a1795b69e3219a468d53f62cf6a0e
Adriano Caldeira <adcaldeira@outlook.com>
2016-05-10 23:05:38 -0300

    ajuste de logotipo

--- Template/Views/Login/Index.cshtml.pp
+++ Template/Views/Login/Index.cshtml.pp
@@ -33,6 +33,7 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="col-md-4">
+								<br/><br />
                                 <img src="~/Content/images/logo.png" class="img-responsive" />
                             </div>
                             <div class="col-md-8">
