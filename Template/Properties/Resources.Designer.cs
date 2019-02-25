a4cf730877652101e30ea070b0cadd3199b3baed
Adriano Caldeira <adcaldeira@outlook.com>
2019-02-25 15:00:50 -0300

    Nova versão

--- Template/Properties/Resources.Designer.cs
+++ Template/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace $rootnamespace$.Properties {
+namespace Template.Properties {
     using System;
     
     
